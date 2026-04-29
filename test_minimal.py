94384141233ba32f84331754eee2eb8995f260a8
Yuvika Malhotra <yuvikamalhotra1414@gmail.com>
2026-04-29 23:32:29 +0530

    docs: add comment to minimal test

--- test_minimal.py
+++ test_minimal.py
@@ -4,3 +4,5 @@ import streamlit as st
 st.set_page_config(page_title="Test", layout="wide")
 st.title("TEST PAGE")
 st.write("If you see this, Streamlit is working.")
+
+# End of minimal test script
